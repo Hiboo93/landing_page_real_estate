6d5f3898d1e55155529ae3f765b3d1d6a4d84e2a
Hiboo93 <99762397+Hiboo93@users.noreply.github.com>
2024-05-14 18:47:05 +0200

    ✨ F09: Creating Faq component and add shadcn accordion component

--- src/components/FeaturedProperties.tsx
+++ src/components/FeaturedProperties.tsx
@@ -12,10 +12,10 @@ const FeaturedProperties = () => {
       <div className="flex flex-wrap">
         {FEATURED_PROPERTIES.map(({name, price, rating, reviews, image}, index) => (
           <div key={index} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
-            <Link href="#" className="m-2 inline-block">
-              <div className="p-8">
+            <Link href="#" className="m-2 inline-block ">
+              <div className="p-8 group">
                 <Image
-                  className="mb-8 rounded-md object-cover"
+                  className="mb-8 rounded-md object-cover group-hover:border-4 border-sky-600"
                   src={image}
                   width={300}
                   height={300}
